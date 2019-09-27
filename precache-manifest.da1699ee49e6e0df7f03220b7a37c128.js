1365540bb790b17c25d89a6b79d884adab5bcd19
youvovk <wer3wollf@gmail.com>
2019-09-27 12:39:25 +0300

    Updates

---
+++ precache-manifest.da1699ee49e6e0df7f03220b7a37c128.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c36ea422dc9da7e5d00d183c2dd6c7",
+    "url": "/react_pagination/index.html"
+  },
+  {
+    "revision": "9a970aed3e5ee0a0ad89",
+    "url": "/react_pagination/static/css/main.f192062a.chunk.css"
+  },
+  {
+    "revision": "f712804033474e80b394",
+    "url": "/react_pagination/static/js/2.762d32b7.chunk.js"
+  },
+  {
+    "revision": "9a970aed3e5ee0a0ad89",
+    "url": "/react_pagination/static/js/main.b930fb8e.chunk.js"
+  },
+  {
+    "revision": "ce8de57aa29cef69bbf2",
+    "url": "/react_pagination/static/js/runtime~main.c851b861.js"
+  }
+]);
